9aca0bbb459c727fa65b3f37ed5555ed83da51aa
peteramelang <git@peteramelang.com>
2026-05-14 11:19:15 +0200

    store: ThemeStore + light/dark/system mode (no FOUC)

--- app/src/stores/index.ts
+++ app/src/stores/index.ts
@@ -1,7 +1,10 @@
 import { LocalStorageProgressStore } from "./LocalStorageProgressStore.js";
 import { LocalStorageBookmarkStore } from "./LocalStorageBookmarkStore.js";
 import { LocalStorageNotesStore } from "./LocalStorageNotesStore.js";
+import { LocalStorageThemeStore } from "./LocalStorageThemeStore.js";
 
 export const progressStore = new LocalStorageProgressStore();
 export const bookmarkStore = new LocalStorageBookmarkStore();
 export const notesStore = new LocalStorageNotesStore();
+export const themeStore = new LocalStorageThemeStore();
+export type { ThemeMode, ResolvedTheme, ThemeStore } from "./ThemeStore.js";
